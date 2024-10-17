2a983636056313194ad5cb98871f299c8cac470b
matheus <matheusnun.ns@gmail.com>
2024-10-17 10:55:18 -0300

    [FIX] change heroku port

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000; // Usa o $PORT fornecido pelo Heroku
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
